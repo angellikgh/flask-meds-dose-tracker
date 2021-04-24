deed80130c65125d6412a13a42f68efa943a8f41
devgeekgh <devgeekgh@gmail.com>
2021-04-24 16:36:39 +0100

    Add register view

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,17 @@
 from tracker import app
 from flask import render_template
+from tracker.forms import RegisterForm
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/register.html', form=form)
+
