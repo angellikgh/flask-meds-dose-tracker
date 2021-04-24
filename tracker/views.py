bba816230a5abc143835bc9031875ede937628a4
devgeekgh <devgeekgh@gmail.com>
2021-04-24 16:40:06 +0100

    Add login view

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,6 @@
 from tracker import app
 from flask import render_template
-from tracker.forms import RegisterForm
+from tracker.forms import RegisterForm, LoginForm
 
 
 @app.route('/')
@@ -15,3 +15,10 @@ def register():
         pass
     return render_template('auth/register.html', form=form)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/login.html', form=form)
