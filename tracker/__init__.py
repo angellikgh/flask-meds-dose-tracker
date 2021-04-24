0b39b38cb721884abaab1a3c9f6e0d5ef4c0b936
devgeekgh <devgeekgh@gmail.com>
2021-04-24 16:42:57 +0100

    Add new installed instance

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 import os
 from dotenv import load_dotenv
 
@@ -7,4 +10,9 @@ load_dotenv()
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_ENV'))
 
-from tracker import views
+Bootstrap(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from tracker import views, models
