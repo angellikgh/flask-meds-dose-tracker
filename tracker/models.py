2539d98e07470965f87873974a38cc802710a422
devgeekgh <devgeekgh@gmail.com>
2021-04-24 16:10:00 +0100

    Add user model

---
+++ tracker/models.py
@@ -0,0 +1,13 @@
+from tracker import db
+
+
+class Users(db.Model):
+    """Users table"""
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name}'
